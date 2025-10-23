7bbd36c540ea81457d5696861f1b8d669d1e0744
oenothera <wjsj1015@naver.com>
2025-10-23 10:18:59 +0900

    fix: 문자 누락 수정
    1. 문자 누락 수정

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -53,7 +53,7 @@ function POST(URL, data = {}, accessToken, isAuth = true, source = "main") {
       return axios.post(host[source] + URL, payload, {
         withCredentials: true,
         headers: {
-          "Content-Type": "apcation/x-www-form-urlencoded;charset=UTF-8",
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
           "X-Requested-With": "XMLHttpRequest",
           Accept: "application/json",
         },
@@ -67,14 +67,14 @@ function POST(URL, data = {}, accessToken, isAuth = true, source = "main") {
         withCredentials: false,
       });
     }
-    
+
     // FormData인 경우 Content-Type 헤더를 설정하지 않음 (브라우저가 자동 설정)
     if (data instanceof FormData) {
       return axios.post(host[source] + URL, data, {
         headers: headers,
       });
     }
-    
+
     return axios.post(host[source] + URL, data, {
       headers: headers,
     });
